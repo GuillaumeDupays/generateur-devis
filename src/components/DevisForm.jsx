5ae25e32b9197aa89e173661f4b827a898ee0d3b
GuillaumeDupays <guillaumedupays@gmail.com>
2020-11-25 10:55:32 +0100

    to do : partie graphique de l'application en essayant flexbox

--- src/components/DevisForm.jsx
+++ src/components/DevisForm.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import Item from "./item";
 import {renderPDFInDOM} from "./pdfMaker";
+import InputText from "./inputText";
 
 class DevisForm extends Component {
     state = {
@@ -17,7 +18,7 @@ class DevisForm extends Component {
         items[id] = {
             id: id,
             description: '',
-            quantite: '',
+            quantite: 1,
             taxe: 5.5,
             amount: 0
         };
@@ -48,24 +49,36 @@ class DevisForm extends Component {
                 <div>
                     <h2>Nouveau devis</h2>
                     <form onSubmit={this.handleSubmit}>
-                        <input value={this.state.id} onChange={evt => this.handleChange(evt, 'id')}
-                               type="text" name="id" id="titre-devis" placeholder="identifiant client" />
-                               <br/>
-                        <input value={this.state.titre} onChange={evt => this.handleChange(evt, 'titre')}
-                               type="text" name="titre-devis" id="titre-devis" placeholder="titre du devis" />
-                               <br/>
-                        <input value={this.state.prenomClient} onChange={evt => this.handleChange(evt, 'prenomClient')}
-                               type="text" name="prenomClient" id="prenomClient" placeholder="prenom du client" />
-                               <br/>
-                        <input value={this.state.nomClient} onChange={evt => this.handleChange(evt, 'nomClient')}
-                               type="text" name="nomClient" id="nomClient" placeholder="nom du client" />
-                               <br/>
+                        <InputText
+                            label="ID"
+                            name="id"
+                            value={this.state.id}
+                            onChange={this.handleChange}
+                        />
+                        <InputText
+                            label="titre"
+                            name="titre"
+                            value={this.state.titre}
+                            onChange={this.handleChange}
+                        />
+                        <InputText
+                            label="prenomClient"
+                            name="prenomClient"
+                            value={this.state.prenomClient}
+                            onChange={this.handleChange}
+                        />
+                        <InputText
+                            label="nomClient"
+                            name="nomClient"
+                            value={this.state.nomClient}
+                            onChange={this.handleChange}
+                        />
                         <button onClick={this.addItem}>Ajouter une ligne</button>
 
                         {Object.keys(this.state.items).map((itemId, index) => (
                             <Item key={index} item={this.state.items[itemId]} onItemChange={this.handleItemChange}/>
                             ))}
-                        <button onClick={() => renderPDFInDOM(JSON.stringify(this.state))}>Générer le devis</button>
+                        <button onClick={() => renderPDFInDOM(this.state)}>Générer le devis</button>
                     </form>
                 </div>
             </React.Fragment>
--- src/components/pdfMaker.jsx
+++ src/components/pdfMaker.jsx
@@ -14,11 +14,53 @@ const styles = StyleSheet.create({
     }
 });
 
-const MyDocument = (props) => (
+const totals = {
+    totalWithoutTaxes: 0,
+    taxes: 0,
+    totalTaxeIncluded: 0
+}
+
+const totalWithoutTaxes = (items) => {
+    const resultWithoutTaxes = Object.keys(items)
+        .map(key => {
+           const amount = parseFloat(items[key].amount, 10);
+           const quantite = parseFloat(items[key].quantite, 10);
+           return amount * quantite;
+        })
+        .reduce((accumulator, current) => accumulator + current );
+        return totals.totalWithoutTaxes = parseFloat(resultWithoutTaxes.toFixed(2));
+}
+const totalTaxeIncluded = (items) => {
+    const resultTaxeIncluded = Object.keys(items)
+        .map(key => {
+            const amount = parseFloat(items[key].amount, 10);
+            const quantite = parseInt(items[key].quantite, 10);
+            const taxe = parseFloat(items[key].taxe, 10);
+            totals.taxes += (amount * quantite * 0.2);
+            const result = ((amount * quantite) + (amount * quantite * taxe));
+            console.log('result', result);
+            return result;
+        })
+        .reduce((acc, curr) => acc + curr, 0);
+    return totals.totalTaxeIncluded = parseFloat(resultTaxeIncluded.toFixed(2));
+}
+const MyDocument = ({text}) => (
     <Document>
         <Page size="A4"  style={styles.page}>
             <View style={styles.section}>
-                <Text>{props.text}</Text>
+                <Text>Devis : {text.titre}</Text>
+                <Text>n° : {text.id}</Text>
+                <Text>Client : {text.prenomClient} {text.nomClient}</Text>
+                <Text>Quantité : {text.quantite}</Text>
+                <Text>Articles : </Text>
+                {Object.keys(text.items).map((key, index) => (
+                    <Text key={key}>
+                        {text.items[key].quantite} &nbsp;
+                        {text.items[key].description} &nbsp;
+                        {parseFloat(text.items[key].taxe) * 100}% &nbsp;
+                        {text.items[key].amount} Euros
+                    </Text>
+                    ))}
             </View>
         </Page>
     </Document>
